c54f9adf668e340dca0a3b3d5ad56e29256168b7
matteosodano <62264708+matteosodano@users.noreply.github.com>
2020-04-30 17:44:56 +0200

    Add files via upload

--- Dummy_Compass/cmake_install.cmake
+++ Dummy_Compass/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/matteo/gap_crossing_OMPL/biped_gap_crossing
+# Install script for directory: /home/matteo/gap_crossing_OMPL/compass_gap_crossing
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/matteo/gap_crossing_OMPL/biped_gap_crossing/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/matteo/gap_crossing_OMPL/compass_gap_crossing/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
